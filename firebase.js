9fef0b0d885ff802f605fa137ca94ce3b6684278
sheralimallah-pixel <sheralimallah@gmail.com>
2026-07-10 14:33:56 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,10 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
 
+const firebaseConfig = {
+  // Yahan apna firebaseConfig paste karo
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
