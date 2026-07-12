e2735379ec8aa550af1989f503bcdf9bbfb95fc5
sheralimallah-pixel <sheralimallah@gmail.com>
2026-07-12 12:45:05 +0500

    Add slideshow functionality to script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Sherry Website Loaded Successfully");
+let slides = document.querySelectorAll(".slide");
+let current = 0;
+
+setInterval(() => {
+    slides[current].classList.remove("active");
+    current = (current + 1) % slides.length;
+    slides[current].classList.add("active");
+}, 3000);
